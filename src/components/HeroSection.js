a3f4ea3132073aa8ad9cfe9191c5c6326a4a77bb
JunaidP24 <junaidpop12@gmail.com>
2023-06-26 02:22:15 -0700

    Adjust hero section for logged in / logged out user

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,16 +2,28 @@ import React from 'react'
 import { Button } from './Button'
 import './HeroSection.css';
 import '../App.css';
-function HeroSection() {
-  return (
-    <div className = 'hero-container'>
-        <h1>Step up your sneaker game</h1>
-        <p>What are you waiting for?</p>
-            <Button className = 'btns' directory='/sign-up' buttonStyle = 'btn--primary'
-            buttonSize='btn--large'>Get Started Now</Button>
+import {useAuth} from '../contexts/AuthContext'
 
-    </div>
-  )
-}
+
+function HeroSection() {
+  const {currentUser} = useAuth()
+    return currentUser ? (
+      <div className = 'hero-container'>
+          <h1>Track your sneaker collection <strong>NOW</strong></h1>
+          <p>What are you waiting for?</p>
+              <Button className = 'btns' directory='/inventory' buttonStyle = 'btn--primary'
+              buttonSize='btn--large'>View Your Inventory</Button>
+  
+      </div> )
+    : (
+      <div className = 'hero-container'>
+          <h1>Step up your sneaker game</h1>
+          <p>What are you waiting for?</p>
+              <Button className = 'btns' directory='/sign-up' buttonStyle = 'btn--primary'
+              buttonSize='btn--large'>Get Started Now</Button>
+  
+      </div>
+    )
+  }
 
 export default HeroSection
